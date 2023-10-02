0b5c1bc34402888f598b6a24b1576c62ac3d5b76
Evgeniy <evgeniy.hamichenok@modsen-software.com>
2023-10-02 22:59:45 +0300

    order in get

--- src/MyWeatherServer/DataAccess.Npgsql/Repositories/LocationRepository.cs
+++ src/MyWeatherServer/DataAccess.Npgsql/Repositories/LocationRepository.cs
@@ -26,6 +26,7 @@ public class LocationRepository : ILocationRepository
         return await _weatherContext
                      .Locations
                      .Where(x => x.UserId == userId)
+                     .OrderBy(x => x.Name)
                      .ProjectTo<Location>(_mapper.ConfigurationProvider)
                      .ToListAsync(cancellationToken);
     }
